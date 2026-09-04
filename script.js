a9db414f2c3f24f971692a28d2276e1a23a2048b
c20245452ramos-lgtm <c20245452.ramos@csav.edu.ph>
2026-09-04 14:44:11 +0800

    Variables (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 console.log("Hello, World!");
 console.log("From script.js file.");
+
+const myName = "Myco";
+let Age = 20;
+Number = "09915129248";
+const Address = "Manapla";
+
+
+
+console.log(`Name is ${myName}`);
+console.log(`Age: ${Age}`);
+console.log(`Number: ${Number}`);
+console.log(`Address: ${Address}`);
+console.log(67);
