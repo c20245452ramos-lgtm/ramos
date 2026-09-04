0478942baadacfb7bed61f87fad01245b48a2ce4
c20245452ramos-lgtm <c20245452.ramos@csav.edu.ph>
2026-09-04 15:40:29 +0800

    functions

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ let Age = 20;
 Number = "09915129248";
 const Address = "Manapla";
 
-
-
-console.log(`Name is ${myName}`);
+console.log(`Name: ${myName}`);
 console.log(`Age: ${Age}`);
 console.log(`Number: ${Number}`);
 console.log(`Address: ${Address}`);
 console.log(67);
+
+function greet(greetings, names = []) {
+    if (!Array.isArray(names)) {
+        
+    }
+
+    return `${greetings} sa enyu ${names}`;
+}
+
+console.log(greet("Mayung aga", ["Mark", "Bai", "Soy", "Tol", "Erp"]));
